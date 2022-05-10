6e4cf5467d34e43319ccb70482b9b602e2a16042
RobbeTh-PXL <12102842@student.pxl.be>
2022-05-10 14:48:34 +0200

    Added user interface

--- Image Smoothing/Img_Smoother.c
+++ Image Smoothing/Img_Smoother.c
@@ -1,7 +1,22 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include "bmp.h"
 
 int main(int argc, char const *argv[]) {
-  printf("Hello World\n");
+  printf("__//The Image Processor\\\\__\n");
+
+//ASK USER FOR INPUT-, OUTPUTFILE, FILTER
+  char *inputfile = NULL;
+  char *outputfile = NULL;
+
+  printf("Path to input file [24 bit BMP]:\n");
+  scanf("%s", inputfile);
+  fflush(stdin);
+
+  printf("Path to output file:\n");
+  scanf("%s", outputfile);
+  fflush(stdin);
+//ASK USER FOR INPUT-, OUTPUTFILE, FILTER
+
   return 0;
 }
