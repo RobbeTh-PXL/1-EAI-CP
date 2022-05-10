60bdb8c5a263cb470d56320bb456b46d7d52944b
RobbeTh-PXL <12102842@student.pxl.be>
2022-05-10 15:08:17 +0200

    Added file handling (open)

--- Image Smoothing/Img_Smoother.c
+++ Image Smoothing/Img_Smoother.c
@@ -18,5 +18,18 @@ int main(int argc, char const *argv[]) {
   fflush(stdin);
 //ASK USER FOR INPUT-, OUTPUTFILE, FILTER
 
+//OPEN INPUT-, OUTPUTFILE
+  FILE *inFile = fopen(inputfile, "r");
+  if (inFile == NULL) {
+    printf("[-] Could not open input file!\n");
+    exit(1);
+  }
+
+  FILE *outFile = fopen(outputfile, "w");
+  if (outFile == NULL) {
+    printf("[-] Could not create output file!\n");
+    exit(2);
+  }
+//OPEN INPUT-, OUTPUTFILE
   return 0;
 }
